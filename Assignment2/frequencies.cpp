c8afaa3e2bcaffbad3af56580adebabe605607a4
SIMONE MUSSINI <284900@studenti.unimore.it>
2025-03-20 09:04:33 +0100

    Solution suggested by our professor

--- Assignment2/frequencies.cpp
+++ Assignment2/frequencies.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <print>
 #include <fstream>
-#include <map>
+#include <iomanip>
 
 int main(int argc, char *argv[])
 {
@@ -17,21 +17,36 @@ int main(int argc, char *argv[])
     return 1;
   }
 
-
   std::ofstream output(argv[2], std::ios::binary);
   if (!output) {
     std::println(std::cerr, "Error opening output file\n");
     return 1;
   }
 
-  std::map <uint8_t, int> occurrencies{};
+  double d = 3.14;
+  double e = static_cast<int>(d);
 
-  while (!input.eof()) {
-    occurrencies[input.get()]++;
+  const unsigned char arr[4] = { 0xaa, 0xbb, 0xcc, 0xdd };
+  const int *p = reinterpret_cast<const int *>(arr);
+  int* m = const_cast<int *>(p);>
+  *m = 0;
+
+
+
+  std::array<size_t, 256> count{};
+
+  input.unsetf(std::ios::skipws);
+  char c;
+  while (input >> c) {
+    uint8_t u = c;  // Implicit cast
+    ++count[u];
   }
 
-  for (auto& [key, value] : occurrencies) {
-    output << key << "\t" << value << "\n";
+  for (int i = 0; i < 256; ++i) {
+    if (count[i] > 0) {
+      output << std::hex << std::setw(2) << std::setfill('0') << i
+        << std::dec << '\t' << count[i] << '\n';
+    }
   }
 
   return 0;
