2e856d9487877ed77810ffc6804533ae40c58ca4
SIMONE MUSSINI <284900@studenti.unimore.it>
2025-03-24 21:31:40 +0100

    Finito di fare assigment 5 con metodo del prof

--- Assignment2/read_int11.cpp
+++ Assignment2/read_int11.cpp
@@ -17,33 +17,70 @@ std::istream& raw_read(std::istream& input, T& val, const size_t size = sizeof(T
     return input.read(reinterpret_cast<char*>(&val), size);
 }
 
+class bitreader {
+    std::istream& is_;
+    uint8_t buffer_ = 0;
+    size_t n_ = 0;
+
+    uint32_t readbit() {
+        if (n_ == 0) {
+            raw_read(is_, buffer_);
+            n_ = 8;
+        }
+        --n_;
+
+        return (buffer_ >> n_) & 1;
+    }
+
+public:
+    bitreader(std::istream& is) : is_(is) {}
+
+    // Read n bits into u from MSB to LSB
+    std::istream& operator()(uint32_t &u, size_t n) {
+        u = 0;
+        while (n --> 0) {
+            u = (u << 1) | readbit();
+        }
+        return is_;
+    }
+};
+
 int main(int argc, char* argv[]) {
     using std::ranges::copy;
 
     if (argc != 3) {
         std::println(std::cerr, "Usage: {} input_file output_file", argv[0]);
-        return 1;
+        return EXIT_FAILURE;
     }
 
     std::ifstream input(argv[1], std::ios::binary);
 
     if (!input) {
         std::println(std::cerr, "Error opening input file");
-        return 1;
+        return EXIT_FAILURE;
     }
 
     std::ofstream output(argv[2]);
 
     if (!output) {
         std::println(std::cerr, "Error opening output file");
-        return 1;
+        return EXIT_FAILURE;
     }
 
-    std::array<char, 11> buffer{};
-    while (raw_read(input, buffer, 1)) {
 
-        copy(buffer, std::ostream_iterator<int>(output, "\n"));
+    bitreader br(input);
+
+    std::vector<int32_t> numbers{};
+    uint32_t value;
+    while (br(value, 11)) {
+        int32_t realnumber = value;
+        if (value > 1023) {
+            realnumber = value - 2048;
+        }
+        numbers.push_back(realnumber);
     }
 
-    return 0;
+    copy(numbers, std::ostream_iterator<int>(output, "\n"));
+
+    return EXIT_SUCCESS;
 }
